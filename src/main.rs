7327ac8944eb037b358a2b9e31a1a7f5caecd365
Leo Pollano <leopollano2011@gmail.com>
2026-02-07 21:55:43 +0100

    v0.1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use sysinfo::{Components, Disks, Networks, System, Pid};
-use std::{env, process, str};
+use std::{env, process, str, string};
 
-
+//conversions
 fn kb_to_gb(kb: u64) -> u64 {
     return (kb) / (1024 * 1024);
+}
+
+fn b_to_gb(b: u64) -> u64 {
+    return (b) / (1024 * 1024 * 1024);
 }
 
 //fn for RAM infos
@@ -50,27 +54,58 @@ fn cpuinfo(sys: &System) -> CpuInfo {
     };
 }
 
-//Process infos
+//disks infos
 
-struct ProcessInfo {
-    pid: Pid,
+struct DiskInfo {
     name: String,
-    cpu_usage: f32,
-    memory_kb: u64,
+    total_space_gb: u64,
+    available_space_gb: u64,
 }
 
-fn processinfo(sys: &System, pid: Pid) -> Option<ProcessInfo> {
-    if let Some(process) = sys.process(pid) {
+fn diskinfo() -> Vec<DiskInfo> {
+    let mut disks_info = Vec::new();
 
-        return Some(ProcessInfo {
-            pid,
-            name: process.name().to_string(),
-            cpu_usage: process.cpu_usage(),
-            memory_kb: process.memory(),
+    let disks = Disks::new_with_refreshed_list();
+
+    for disk in disks.list() {
+        let name = disk.name().to_string_lossy().into_owned();
+        let total_space_gb = b_to_gb(disk.total_space());
+        let available_space_gb = b_to_gb(disk.available_space());
+
+        disks_info.push(DiskInfo {
+            name,
+            total_space_gb,
+            available_space_gb,
         });
     }
-    None
+
+    return disks_info;
 }
+
+//Networks infos
+
+struct NetworkInfo {
+    interface_name: String,
+    received_bytes: u64,
+    transmitted_bytes: u64,
+}
+
+fn networkinfo() -> Vec<NetworkInfo> {
+    let networks = Networks::new_with_refreshed_list();
+
+    let net_infos: Vec<NetworkInfo> = networks.list().iter().map(|(name, data)| {
+        NetworkInfo {
+            interface_name: name.to_string(),
+            received_bytes: data.received(),
+            transmitted_bytes: data.transmitted(),
+        }
+    }).collect();
+
+    return net_infos;
+    
+}
+
+//main
 
 fn main(){
     let mut sys = System::new_all();
@@ -92,6 +127,7 @@ fn main(){
     println!("Used RAM: {} GB", ram.used_gb);
     println!("Total Swap: {} GB", ram.total_swap_gb);
     println!("Used Swap: {} GB", ram.used_swap_gb);
+    println!("-----------------------------");
 
     //CPU
 
@@ -104,12 +140,25 @@ fn main(){
     println!("CPU Usage: {:.2}%", cpu.usage_percent);
     println!("Number of CPUs: {}", cpu.num_cpus);
     println!("CPU Frequency: {} MHz", cpu.frequency_mhz);
+    println!("-----------------------------");
 
     //disks
 
-    let disks = Disks::new_with_refreshed_list();
-    for disk in disks.list() {
-    println!("{disk:?}");
+    let disks = diskinfo();
+    for disk in disks {
+        println!("Disk Name: {}", disk.name);
+        println!("Total Space: {} GB", disk.total_space_gb);
+        println!("Available Space: {} GB", disk.available_space_gb);
+        println!("-----------------------------");
+    }
+
+    //networks
+    let networks = networkinfo();
+    for net in networks {
+        println!("Interface: {}", net.interface_name);
+        println!("Received Bytes: {} bytes", net.received_bytes);
+        println!("Transmitted Bytes: {} bytes", net.transmitted_bytes);
+        println!("-----------------------------");
     }
 
     //Processes
@@ -130,6 +179,7 @@ fn main(){
             
             println!("PID:{}\tName:{} :\n CPU:{}%\tRAM:{} KB", 
                      pid, process.name(), process.cpu_usage(), process.memory());
+                     println!("**********************************")
         }
         
     }
